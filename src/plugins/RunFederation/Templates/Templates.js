4d4cde3691ae98d88aeff12bc7d50cffabc994b6
Yogesh Barve <ydbarve@isis.vanderbilt.edu>
2016-08-17 21:05:07 +0000

    fleshing out the demo code.

--- src/plugins/RunFederation/Templates/Templates.js
+++ src/plugins/RunFederation/Templates/Templates.js
@@ -2,5 +2,5 @@
 /* Generated file based on ejs templates */
 define([], function() {
     return {
-    "dockerFileTemplate.ejs": "--- \nservices: \n<%\nfedInfos.map(function(fedInfo) {\n-%>\n  <%- fedInfo.type %>_<%- fedInfo.name %>:\n    build: .\n    image: \"<%- dockerInstanceName %>:<%- tag %>\"\n    command: \"mvn package exec:exec -P <%- fedInfo.type %>,<%- fedInfo.name %>\" \n    volumes:\n      - ./input:/root/Projects/c2wt/input\n      - ./output:/root/Projects/c2wt/output\n<%\n})\n-%>\nversion: \"2\"\n"
+    "dockerFileTemplate.ejs": "--- \nservices: \n  fedManager:\n    build: .\n    image: \"ydbarve/c2wtcore_v002:160816\"\n    command: \"xvfb-run -a mvn package exec:exec -P FedManager\"\n    volumes:\n      - <%- inputPrefix %>:/root/Projects/c2wt/input\n      - <%- outputPrefix %>:/root/Projects/c2wt/logs\n<%\nfedInfos.map(function(fedInfo) {\n-%>\n  <%- fedInfo.type %>_<%- fedInfo.name %>:\n    build: .\n    image: \"<%- dockerInfoMap[fedInfo.type].name %>:<%- dockerInfoMap[fedInfo.type].tag %>\"\n    command: \"mvn package exec:exec -P <%- fedInfo.type %>,<%- fedInfo.name %>\" \n    volumes:\n      - <%- inputPrefix %>:/root/Projects/c2wt/input\n      - <%- outputPrefix %>:/root/Projects/c2wt/logs\n<%\n})\n-%>\nversion: \"2\"\n"
 }});
