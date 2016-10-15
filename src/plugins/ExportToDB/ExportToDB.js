c05a55381e0c2d6c24446b14bdefa03917c55779
William Emfinger <waemfinger@gmail.com>
2016-10-15 12:02:24 -0500

    updated to have some parts of process model implemented to start converting to db format.  federates are generated with GUIDs and Versions and placed into the properly locations.

--- src/plugins/ExportToDB/ExportToDB.js
+++ src/plugins/ExportToDB/ExportToDB.js
@@ -76,10 +76,11 @@ define([
 	
 	webgmeToJSON.notify = function(level, msg) {self.notify(level, msg);}
 
-	// don't resolve pointers, don't keep webgmeNodes as part of the JSON.
-	webgmeToJSON.loadModel(self.core, self.activeNode, false, false)
+	// resolve pointers and children, don't keep webgmeNodes as part of the JSON.
+	webgmeToJSON.loadModel(self.core, self.activeNode, true, false)
 	    .then(function(model) {
-		return self.blobClient.putFile(nodeName+'.json', JSON.stringify(model.objects, null, 2));
+		model = self.processModel(model); // convert to the DB format
+		return self.blobClient.putFile(nodeName+'.json', JSON.stringify(model, null, 2));
 	    })
 	    .then(function(hash) {
 		self.result.addArtifact(hash);
@@ -92,6 +93,155 @@ define([
 		callback(err, self.result);
 	    })
 		.done();
+    };
+
+    ExportToDB.prototype.processModel = function(model) {
+	var self = this;
+	// THIS FUNCTION HANDLES CREATION OF SOME CONVENIENCE MEMBERS
+	// FOR SELECT OBJECTS IN THE MODEL
+
+	// Need to create:
+	//  - GUIDs for all objects (for now)
+	//    - will be handled by DB intf
+	//  - Versions for relevant objects (for now)
+	//    - will be handled by DB intf
+	//  - __OBJECTS__ list
+	//    - will be handled by DB intf
+	//  - <type> lists of GUID references
+	//    - will be handled by DB intf
+
+	//  - dummy users and organizations?
+	//    - will be handled by other subsystems
+	//  - dummmy docker images and repositories?
+	//    - will be handled by other subsystems
+
+	// Need to convert:
+	//  - federations to heirarchical federates
+	//    - actually has to be done
+	//  - all objects to __OBJECTS__ notation from example.js
+	//    - actually has to be done
+	//  - poitners to GUID references
+	//    - will have to see exactly how this works, where we
+	//      really get GUIDs from
+
+	// for testing, need the structure that would be in the database
+	self.initDB(model);
+
+	self.buildFederateTree(model);
+	self.extractParameters(model);
+	self.extractInteractions(model);
+	self.extractCOAs(model);
+	self.extractExperiments(model);
+	self.extractConfigurations(model);
+
+	// for testing since we will need these other objects
+	self.buildDummyObjects(model);
+
+	// now that we've transformed the model, get rid of the
+	// original data
+	model = model._DB;
+	return model;
+    };
+
+    ExportToDB.prototype.initDB = function(model) {
+	model._DB = {
+	    "__OBJECTS__": {},
+	    "projects": [],
+	    "Users": [],
+	    "organizations": [],
+	    "Federates": [],
+	    "coas": [],
+	    "Experiments": [],
+	    "Interactions": [],
+	    "Parameters": [],
+	    "repositories": [],
+	    "builds": [],
+	    "executions": [],
+	    "Docker Images": []
+	}
+    };
+
+    ExportToDB.prototype.buildFederateTree = function(model) {
+	var self = this;
+	// to iterate through the model we need to know exactly
+	// what the META is
+
+	console.log(model);
+	
+	// iterate through all the FOM Sheets in the root node
+	model.root.FOMSheet_list.map(function(FOMSheetInfo) {
+	    // Need to go through all children of the FOMSheet
+	    // which are federates, should refactor a little so
+	    // that there is just a single Federate_list which
+	    // contains all types of federates.  will require some
+	    // further processing.
+	    var fedList = []
+	    fedList = fedList.concat(FOMSheetInfo.Federate_list);
+	    fedList = fedList.concat(FOMSheetInfo.CPNFederate_list);
+	    fedList = fedList.concat(FOMSheetInfo.OmnetFederate_list);
+	    fedList = fedList.concat(FOMSheetInfo.MapperFederate_list);
+	    fedList = fedList.concat(FOMSheetInfo.JavaFederate_list);
+	    fedList = fedList.concat(FOMSheetInfo.CppFederate_list);
+	    fedList = fedList.concat(FOMSheetInfo.GridlabDFederate_list);
+	    fedList.map(function(fedInfo) {
+		var newObj = self.transformFederate(fedInfo);
+		if (newObj) {
+		    newObj.GUID = self.generateGUID(); // wouldn't actually happen in the plugin
+		    self.generateVersion(newObj);
+		    model._DB.__OBJECTS__[newObj.GUID] = {};
+		    model._DB.__OBJECTS__[newObj.GUID][newObj.version] = newObj;
+		    model._DB.Federates.push({ GUID: newObj.GUID, version: newObj.version });
+		}
+	    });
+	});
+    };
+
+    ExportToDB.prototype.transformFederate = function(obj) {
+	// converts from the generic representation we have here
+	// to the specific representation given in example.js
+	if (obj == null)
+	    return null;
+	var newObj = {};
+	var attrNames = Object.keys(obj.attributes);
+	attrNames.map(function(attrName) {
+	    newObj[attrName] = obj.attributes[attrName];
+	});
+	return newObj;
+    };
+
+    ExportToDB.prototype.extractParameters = function(model) {
+    };
+
+    ExportToDB.prototype.extractInteractions = function(model) {
+    };
+
+    ExportToDB.prototype.extractCOAs = function(model) {
+    };
+
+    ExportToDB.prototype.extractExperiments = function(model) {
+    };
+
+    ExportToDB.prototype.extractConfigurations = function(model) {
+    };
+
+    ExportToDB.prototype.buildDummyObjects = function(model) {
+    };
+
+    ExportToDB.prototype.generateGUID = function() {
+	var d = new Date().getTime();
+	if(window.performance && typeof window.performance.now === "function"){
+            d += performance.now(); //use high-precision timer if available
+	}
+	var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (d + Math.random()*16)%16 | 0;
+            d = Math.floor(d/16);
+            return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+	});
+	return uuid;
+    };
+
+    ExportToDB.prototype.generateVersion = function(object) {
+	object.version = (object.version | '1.0') + '.1';
     };
 
     return ExportToDB;
