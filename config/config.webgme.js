e1084697ebcc8d3e8f94527fdb94deaa5a2e3c0c
Yogesh Barve <ydbarve@isis.vanderbilt.edu>
2018-03-23 20:27:49 +0000

    #74 update to the webgme config

--- config/config.webgme.js
+++ config/config.webgme.js
@@ -6,36 +6,25 @@
 var config = require('webgme/config/config.default'),
     validateConfig = require('webgme/config/validator');
 
+
 // The paths can be loaded from the webgme-setup.json
-config.plugin.basePaths.push(__dirname + '/../src/plugins');
-config.visualization.decoratorPaths.push(__dirname + '/../src/decorators');
+config.plugin.basePaths.push('src/plugins');
+config.visualization.decoratorPaths.push('src/decorators');
 
 
 
-config.visualization.panelPaths.push(__dirname + '/../node_modules/webgme-codeeditor/src/visualizers/panels');
-
-
-// allow server side execution
-config.plugin.allowServerExecution = true
+config.visualization.panelPaths.push('node_modules/webgme-codeeditor/src/visualizers/panels');
 
 
 // Visualizer descriptors
-config.visualization.visualizerDescriptors.push(__dirname + '/../src/visualizers/Visualizers.json');
+config.visualization.visualizerDescriptors.push('./src/visualizers/Visualizers.json');
 // Add requirejs paths
 config.requirejsPaths = {
   'CodeEditor': 'panels/CodeEditor/CodeEditorPanel',
   'panels': './src/visualizers/panels',
   'widgets': './src/visualizers/widgets',
-  'c2wtng': './src/common',
-  'C2Core': './src/plugins/C2Core',
-  'FederatesExporter': './src/plugins/FederatesExporter',
-  'DeploymentExporter': './src/plugins/DeploymentExporter',
-  'C2Federates': './src/plugins/C2Federates',
-  'RunFederation':'./src/plugins/RunFederation',
   'panels/CodeEditor': './node_modules/webgme-codeeditor/src/visualizers/panels/CodeEditor',
-  'widgets/CodeEditor': './node_modules/webgme-codeeditor/src/visualizers/widgets/CodeEditor',
-   "combinatorics": './node_modules/js-combinatorics/'
-
+  'widgets/CodeEditor': './node_modules/webgme-codeeditor/src/visualizers/widgets/CodeEditor'
 };
 
 
