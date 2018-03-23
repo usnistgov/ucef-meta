ca5dc9ea950689035870196eaaf0021ec9646a69
Yogesh Barve <ydbarve@isis.vanderbilt.edu>
2018-03-23 21:29:11 +0000

    #74 updated the config

--- config/config.dstudio.js
+++ config/config.dstudio.js
@@ -5,6 +5,44 @@ var config = require('./config.webgme'),
     validateConfig = require('webgme/config/validator');
 
 
+// The paths can be loaded from the webgme-setup.json
+config.plugin.basePaths.push(__dirname + '/../src/plugins');
+config.visualization.decoratorPaths.push(__dirname + '/../src/decorators');
+config.visualization.panelPaths.push(__dirname + '/../node_modules/webgme-codeeditor/src/visualizers/panels');
+// allow server side execution
+config.plugin.allowServerExecution = true
+// Visualizer descriptors
+config.visualization.visualizerDescriptors.push(__dirname + '/../src/visualizers/Visualizers.json');
+
+config.visualization.decoratorPaths.push('./src/decorators');
+config.seedProjects = {
+            enable: true,
+            allowDuplication: true, //requires mongodb >= 2.6
+            defaultProject: 'BasicCASIM',
+            basePaths: [path.join(__dirname, '../seeds')]
+        };
+
+config.client.log.level = 'debug';
+
+// Add requirejs paths
+config.requirejsPaths = {
+  'CodeEditor': 'panels/CodeEditor/CodeEditorPanel',
+  'panels': './src/visualizers/panels',
+  'widgets': './src/visualizers/widgets',
+  'c2wtng': './src/common',
+  'C2Core': './src/plugins/C2Core',
+  'FederatesExporter': './src/plugins/FederatesExporter',
+  'DeploymentExporter': './src/plugins/DeploymentExporter',
+  'C2Federates': './src/plugins/C2Federates',
+  'RunFederation':'./src/plugins/RunFederation',
+  'panels/CodeEditor': './node_modules/webgme-codeeditor/src/visualizers/panels/CodeEditor',
+  'widgets/CodeEditor': './node_modules/webgme-codeeditor/src/visualizers/widgets/CodeEditor',
+   "combinatorics": './node_modules/js-combinatorics/',
+   'webgme-to-json':"./node_modules/webgme-to-json/"
+};
+
+
+
 // Enable Authentication
 config.authentication.enable = true;
 config.authentication.inferredUsersCanCreate = true;
