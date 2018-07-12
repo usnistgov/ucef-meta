d8597ae6dfd3ce73e735cf401212fd87ed893e1d
Himanshu Neema <himanshu.neema@vanderbilt.edu>
2018-07-12 15:33:01 +0000

    Fixes issue #71 about code generation despite the absence of any experiments

--- src/plugins/DeploymentExporter/DeploymentExporter.js
+++ src/plugins/DeploymentExporter/DeploymentExporter.js
@@ -706,28 +706,29 @@ define([
 
     ///--------------------------------------
     // list of experiments.json
-    self.fileGenerators.push(function (artifact, callback) {
+    if (self.experimentPaths.length != 0) {
 
-        var experimentlist = {}
+        self.fileGenerators.push(function (artifact, callback) {
 
-        // var experimentlist = []
-        if (self.experimentPaths.length != 0) {
-             self.experimentPaths.forEach(function (objPath) {
-                 self.experimentModelConfig[objPath].forEach(function (expSet) {
-                    experimentlist[self.core.getAttribute(self.core.getParent(expSet), "name").toLowerCase()] = self.core.getAttribute(self.core.getParent(expSet), "name").toLowerCase()
+            var experimentlist = {}
+
+                 self.experimentPaths.forEach(function (objPath) {
+                     self.experimentModelConfig[objPath].forEach(function (expSet) {
+                        experimentlist[self.core.getAttribute(self.core.getParent(expSet), "name").toLowerCase()] = self.core.getAttribute(self.core.getParent(expSet), "name").toLowerCase()
+                     })
                  })
-             })
 
-            artifact.addFile('conf/' + 'experimentlist.json', JSON.stringify(experimentlist, null, 2), function (err) {
-            if (err) {
-                callback(err);
-                return;
-            } else {
-                callback();
-            }
-        });     
-        }
-    })
+                artifact.addFile('conf/' + 'experimentlist.json', JSON.stringify(experimentlist, null, 2), function (err) {
+                if (err) {
+                    callback(err);
+                    return;
+                } else {
+                    callback();
+                }
+            });     
+        });
+    }
+
 
 
 
