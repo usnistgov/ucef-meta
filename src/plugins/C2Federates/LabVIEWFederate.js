0df32e0ad0e1f475fa6d4752bd3bf5693679e174
Thomas Roth <thomas.roth@nist.gov>
2018-09-06 20:12:10 +0000

    updated labview generated pom.xml

--- src/plugins/C2Federates/LabVIEWFederate.js
+++ src/plugins/C2Federates/LabVIEWFederate.js
@@ -20,13 +20,19 @@ define([
                 nodeType = self.core.getAttribute( self.getMetaType(node), 'name' );
 
             context['labviewfedspec'] = {};
+            context['labviewfedspec']['groupId'] = self.mainPom.groupId.trim();
             context['labviewfedspec']['projectName'] = self.projectName;
             context['labviewfedspec']['projectVersion'] = self.project_version;
             context['labviewfedspec']['classname'] = self.core.getAttribute(node, 'name');
             context['labviewfedspec']['lookahead'] = self.core.getAttribute(node, 'Lookahead');
             context['labviewfedspec']['step'] = self.core.getAttribute(node, 'Step');
             context['labviewfedspec']['bindAddress'] = self.getCurrentConfig().bindAddress.trim();
+            context['labviewfedspec']['rootdir'] = self.getCurrentConfig().codebaseDirectory.trim();
             context['labviewfedspec']['jarfile'] = labviewArtifactId + "-" + labviewVersion + ".jar";
+            context['labviewfedspec']['labviewPOM'] = {};
+            context['labviewfedspec']['labviewPOM']['artifactId'] = labviewArtifactId;
+            context['labviewfedspec']['labviewPOM']['groupId'] = labviewGroupId;
+            context['labviewfedspec']['labviewPOM']['version'] = labviewVersion;
 
             self.federates[self.core.getPath(node)] = context['labviewfedspec'];
 
@@ -39,66 +45,9 @@ define([
                 moduleName = renderContext['classname'],
                 configDirectory = moduleName + "/conf";
 
-            // reference to the LabVIEW wrapper
-            var labviewPOM = new MavenPOM();
-            labviewPOM.artifactId = labviewArtifactId;
-            labviewPOM.groupId = labviewGroupId;
-            labviewPOM.version = labviewVersion;
-
-            // pom.xml that fetches the resources required to run the LabVIEW federate
-            // the maven-dependency-plugin will pull in the LabVIEW wrapper code
-            // the maven-resources-plugin will pull in the FederatesExporter FOM.xml file
-            var labviewHelperPOM = new MavenPOM(self.mainPom);
-            labviewHelperPOM.artifactId = moduleName;
-            labviewHelperPOM.version = renderContext['projectVersion'];
-            labviewHelperPOM.dependencies.push(labviewPOM);
-            labviewHelperPOM.plugins.push({
-                    'groupId': {'#text': 'org.apache.maven.plugins'},
-                    'artifactId': {'#text': 'maven-dependency-plugin'},
-                    'version': {'#text': '3.1.1'},
-                    'executions': {
-                        'execution': {
-                            'id': {'#text': 'copy-dependencies'},
-                            'phase': {'#text': 'package'},
-                            'goals': {
-                                'goal': {'#text': 'copy-dependencies'}
-                            },
-                            'configuration': {
-                                'outputDirectory': {'#text': '${project.basedir}'},
-                                'overWriteReleases': {'#text': 'true'},
-                                'overWriteSnapshots': {'#text': 'true'},
-                                'overWriteIfNewer': {'#text': 'true'}
-                            }
-                        }
-                    }
-                });
-            labviewHelperPOM.plugins.push({
-                    'groupId': {'#text': 'org.apache.maven.plugins'},
-                    'artifactId': {'#text': 'maven-resources-plugin'},
-                    'version': {'#text': '3.1.0'},
-                    'executions': {
-                        'execution': {
-                            'id': {'#text': 'copy-resources'},
-                            'phase': {'#text': 'package'},
-                            'goals': {
-                                'goal': {'#text': 'copy-resources'}
-                            },
-                            'configuration': {
-                                'outputDirectory': {'#text': '${basedir}/conf'},
-                                'resources': {
-                                    'resource': {
-                                        'directory': {'#text': '${basedir}/../fom'},
-                                    }
-                                },
-                                'overwrite': {'#text': 'false'}
-                            }
-                        }
-                    }
-                });
-
             // generate the pom.xml that fetches the LabVIEW federate code and resources
             self.fileGenerators.push(function (artifact, callback) {
-                artifact.addFile(moduleName + "/pom.xml", self._jsonToXml.convertToString(labviewHelperPOM.toJSON()), function(err) {
+                artifact.addFile(moduleName + "/pom.xml", ejs.render(TEMPLATES['java/labview-pom.xml.ejs'], renderContext), function(err) {
                     if (err) {
                         callback(err);
                         return;
