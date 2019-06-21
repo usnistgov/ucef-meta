dcb48aac75cde3dc8befce7ccbe7616cb022c496
Tom Kramer <kramer@nist.gov>
2019-06-21 19:55:58 +0000

    minor reformatting

--- src/plugins/C2Federates/JavaFederate.js
+++ src/plugins/C2Federates/JavaFederate.js
@@ -6,10 +6,10 @@ define
   'C2Federates/JavaBaseFederate',
   'C2Federates/JavaImplFederate'],
  function (ejs,
-	   MavenPOM,
-	   TEMPLATES,
-	   JavaBaseFederate,
-	   JavaImplFederate)
+           MavenPOM,
+           TEMPLATES,
+           JavaBaseFederate,
+           JavaImplFederate)
  {
 
     'use strict';
@@ -18,6 +18,12 @@ define
 /***********************************************************************/
 
 /* JavaFederateExporter (function-valued variable of top-level function object)
+
+Returned Value: none
+
+Called By: FederatesExporter in FederatesExporter.js
+
+The top-level function returns this function.
 
 */
     
@@ -30,47 +36,64 @@ define
        
 /***********************************************************************/
 
+/* visit_JavaFederate
+
+Returned Value: a "context" object returned by visit_JavaImplFederate,
+which appears to be intended to pass to an ejs thingy for rendering.
+
+Called By: visit_MapperFederate in MapperFederate.js 
+           This may also be called by functions that call a function whose
+           name is made by concatentating 'visit_' with other strings.
+           The getVisitorFuncName function in FederatesExporter makes
+           function names that way.
+
+This does not appear to be called unless the node type name is JavaFederate.
+In visit_MapperFederate it is called only if it is defined. It is defined
+if JavaFederateExporter is called, which happens in FederatesExporter.js.
+
+*/
+
       this.visit_JavaFederate = function(node, parent, context)
       {
-	var self;
-	var nodeType;
+        var self;
+        var nodeType; // set here but not used here, may be useless
   
-	self = this;
-	nodeType = self.core.getAttribute(self.getMetaType(node), 'name');
-	self.logger.info('Visiting the JavaFederates');
-	if (!self.javaPOM)
-	  {
-	    self.javaPOM = new MavenPOM(self.mainPom);
-	    self.javaPOM.artifactId = self.projectName + "-java-federates";
-	    self.javaPOM.directory = self.projectName + "-java-federates";
-	    self.javaPOM.version = self.project_version;
-	    self.javaPOM.addMavenCompiler(self.getCurrentConfig().
-					  mavenCompilerPluginJavaVersion);
-	    self.javaPOM.packaging = "pom";
-	    self.javaPOM.dependencies.push(self.porticoPOM);
-	  }
-	if (!self.porticoPOM)
-	  {  
-	    self.porticoPOM = new MavenPOM();
-	    self.porticoPOM.artifactId = "portico";
-	    self.porticoPOM.groupId = "org.porticoproject";
-	    // Set the portico Release Version
-	    self.porticoPOM.version =
-	      self.getCurrentConfig().porticoReleaseNum;
-	    self.porticoPOM.scope = "provided";
-	  }
+        self = this;
+        nodeType = self.core.getAttribute(self.getMetaType(node), 'name');
+        self.logger.info('Visiting the JavaFederates');
+        if (!self.javaPOM)
+          {
+            self.javaPOM = new MavenPOM(self.mainPom);
+            self.javaPOM.artifactId = self.projectName + "-java-federates";
+            self.javaPOM.directory = self.projectName + "-java-federates";
+            self.javaPOM.version = self.project_version;
+            self.javaPOM.addMavenCompiler(self.getCurrentConfig().
+                                          mavenCompilerPluginJavaVersion);
+            self.javaPOM.packaging = "pom";
+            self.javaPOM.dependencies.push(self.porticoPOM);
+          }
+        if (!self.porticoPOM)
+          {  
+            self.porticoPOM = new MavenPOM();
+            self.porticoPOM.artifactId = "portico";
+            self.porticoPOM.groupId = "org.porticoproject";
+            // Set the portico Release Version
+            self.porticoPOM.version =
+              self.getCurrentConfig().porticoReleaseNum;
+            self.porticoPOM.scope = "provided";
+          }
 
-	this.visit_JavaBaseFederate(node, parent, context);
-	return this.visit_JavaImplFederate(node, parent, context);
+        this.visit_JavaBaseFederate(node, parent, context);
+        return this.visit_JavaImplFederate(node, parent, context);
       };
 
 /***********************************************************************/
 
       this.post_visit_JavaFederate = function(node, context)
       {
-	this.post_visit_JavaBaseFederate(node, context);
-	finalContext = this.post_visit_JavaImplFederate(node, context);
-	return finalContext;
+        this.post_visit_JavaBaseFederate(node, context);
+        finalContext = this.post_visit_JavaImplFederate(node, context);
+        return finalContext;
       };
 
 /***********************************************************************/
