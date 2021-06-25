2cb0cba44a737e4d41d96bf5db0c7a380b3e90cb
vagrant <vagrant@vagrant.vm>
2021-06-25 12:09:13 -0500

    Updates for CPSWTMeta library

--- config/config.webgme.js
+++ config/config.webgme.js
@@ -6,25 +6,28 @@
 var config = require('webgme/config/config.default'),
     validateConfig = require('webgme/config/validator');
 
-
 // The paths can be loaded from the webgme-setup.json
-config.plugin.basePaths.push('src/plugins');
-config.visualization.decoratorPaths.push('src/decorators');
+config.plugin.basePaths.push(__dirname + '/../src/plugins');
+config.visualization.decoratorPaths.push(__dirname + '/../src/decorators');
 
 
 
-config.visualization.panelPaths.push('node_modules/webgme-codeeditor/src/visualizers/panels');
+config.visualization.panelPaths.push(__dirname + '/../node_modules/webgme-codeeditor/src/visualizers/panels');
+
+
 
 
 // Visualizer descriptors
-config.visualization.visualizerDescriptors.push('./src/visualizers/Visualizers.json');
+config.visualization.visualizerDescriptors.push(__dirname + '/../src/visualizers/Visualizers.json');
 // Add requirejs paths
 config.requirejsPaths = {
   'CodeEditor': 'panels/CodeEditor/CodeEditorPanel',
   'panels': './src/visualizers/panels',
   'widgets': './src/visualizers/widgets',
   'panels/CodeEditor': './node_modules/webgme-codeeditor/src/visualizers/panels/CodeEditor',
-  'widgets/CodeEditor': './node_modules/webgme-codeeditor/src/visualizers/widgets/CodeEditor'
+  'widgets/CodeEditor': './node_modules/webgme-codeeditor/src/visualizers/widgets/CodeEditor',
+  'webgme-codeeditor': './node_modules/webgme-codeeditor/src/common',
+  'c2-ng_modeler': './src/common'
 };
 
 
